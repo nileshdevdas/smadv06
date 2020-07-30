8359d593062779445c415d7ef2956add9d8cca8f
nilesh.devdas@vinsys.com <nilesh.devdas@vinsys.com>
2020-07-30 10:11:40 +0530

    Metrics and Dashboard

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // imports the aws-sdk 
 var AWS = require("aws-sdk");
+
+// S3 //
 var S3 = new AWS.S3();
+// S3 // 
+
+// Cloud Watch // 
+var CloudWatch = new AWS.CloudWatch({
+    region: 'ap-south-1'
+});
+// Cloud Watch //
 
 exports.handler = (event) => {
     //console.log(event);
@@ -17,9 +26,34 @@ exports.handler = (event) => {
         } else {
             var content = res.Body.toString();
             var lines = content.split("\n");
+            // counter 
+            var linecount = 0;
             lines.forEach((eachLine) => {
                 var cols = eachLine.split(",");
                 console.log(cols[3]);
+                linecount = linecount + 1;
+            });
+            console.log("Total Recordsd processd ", linecount);
+            console.log("---------------------Publishing Metrics------------------")
+            // Metric Construct // 
+            var metric = {
+                MetricData: [{
+                    MetricName: 'TOTAL_RECORDS',
+                    Dimensions: [{
+                        Name: 'FILE_PROCESSING',
+                        Value: 'RECORDS_PROCESSED'
+                    }, ],
+                    Timestamp: new Date(),
+                    Unit: 'Count',
+                    Value: linecount
+                }],
+                Namespace: 'nilapp'
+            };
+            CloudWatch.putMetricData(metric, (_perr, presp) => {
+                if (_perr)
+                    console.log(_perr);
+                else
+                    console.log("published metrics succesfully");
             });
         }
     });
@@ -31,3 +65,17 @@ exports.handler = (event) => {
     return response;
 };
 
+
+var event = {
+    Records: [{
+        s3: {
+            bucket: {
+                name: 'vinsys.com.lambdanilesh'
+            },
+            object: {
+                key: 'airports.csv'
+            }
+        }
+    }]
+};
+console.log(this.handler(event));
